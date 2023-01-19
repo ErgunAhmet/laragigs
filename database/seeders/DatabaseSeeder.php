37d3acf96ad832fabb1c26b103e7f797f11e2616
ahmet <80220613+ErgunAhmet@users.noreply.github.com>
2023-01-19 18:10:30 +0100

    building ui 2

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
          \App\Models\User::factory(10)->create();
 
+         Listing::factory(6) ->create();
+        /*
         Listing::create(
             [
                 'title' => 'Laravel Senior Developer', 
@@ -40,6 +43,7 @@ class DatabaseSeeder extends Seeder
                 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?'
               ]
         );
+        */
 
     }
 }
